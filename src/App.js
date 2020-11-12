54ff35d85f6c7f44be92365d545952817f3d8b2e
Maya Chisem <chisem.maya@gmail.com>
2020-11-12 08:52:20 -0800

    making the homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Discover from "./pages/Discover";
+import Home from "./pages/Home";
+// import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          {/* <Route exact path="/discover" component={Discover} /> */}
+          {/* <Route exact path="/search" component={Search} /> */}
+        </Wrapper>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import '../App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import { MDBIcon } from "mdbreact";
+
+function Homepage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Get the app and order now.
+          <br></br>
+          <a
+          className="app-download"
+          id="apple-download"
+          href="https://www.apple.com/?afid=p238%7CseIEs444j-dc_mtid_1870765e38482_pcrid_479177971632_pgrid_13945964887_&cid=aos-us-kwgo-brand-apple--slid---product-"
+          target="apple"
+          rel="apple download"
+          >
+            <MDBIcon fab icon="apple" />
+          </a>
+          <a
+          className="app-download"
+          id="android-apple"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+            <MDBIcon fab icon="android" />
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
