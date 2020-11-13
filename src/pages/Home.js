f8307246f59164d4cb907a2fb0a01b7d00183275
Maya Chisem <chisem.maya@gmail.com>
2020-11-12 22:51:13 -0800

    still trying to deploy on heroku

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import { MDBIcon } from "mdbreact";
+// import { MDBIcon } from "mdbreact";
 import MediaObject from '../components/MediaObject';
-import Works from '../components/Works';
-import Features from '../components/Features';
-import Search from '../components/Search';
-import Footer from '../components/Footer';
+// import Works from '../components/Works';
+// import Features from '../components/Features';
+// import Search from '../components/Search';
+// import Footer from '../components/Footer';
+import AppleIcon from '../components/AppleIcon';
+import AndroidIcon from '../components/AndroidIcon';
 import '../App.css';
 
 function Homepage() {
@@ -17,7 +19,9 @@ function Homepage() {
         <p>
           Get the app and order now.
           <br></br>
-          <a
+          <AppleIcon></AppleIcon>
+          <AndroidIcon></AndroidIcon>
+          {/* <a
           className="app-download"
           id="apple-download"
           href="https://www.apple.com/?afid=p238%7CseIEs444j-dc_mtid_1870765e38482_pcrid_479177971632_pgrid_13945964887_&cid=aos-us-kwgo-brand-apple--slid---product-"
@@ -34,14 +38,14 @@ function Homepage() {
           rel="noopener noreferrer"
           >
             <MDBIcon fab icon="android" />
-          </a>
+          </a> */}
         </p>
       </header>
       <MediaObject></MediaObject>
-      <Works></Works>
+      {/* <Works></Works>
       <Features></Features>
       <Search></Search>
-      <Footer></Footer>
+      <Footer></Footer> */}
     </div>
   );
 }
